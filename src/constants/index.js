51cb396d459d4790b68c81096755e2aabe3efae2
Tinotenda Fred <fredtinotenda3@gail.com>
2024-12-06 17:38:57 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,222 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Prediction Model For Generator Diseal",
+    company_name: "Binder-Abattoir",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "March 2020 - April 2021",
+    points: [
+      "The Prediction Model for Generator Diesel Consumption is an innovative solution designed to optimize diesel usage in generator operations. This model leverages advanced machine learning algorithms and data analysis techniques to forecast diesel consumption based on various factors such as generator load, runtime, environmental conditions, and historical usage patterns.",
+    ],
+  },
+  {
+    title: "Intern React Native Developer",
+    company_name: "Tesla",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "As a React Native Developer, I have extensive experience in designing, developing, and deploying mobile applications across iOS and Android platforms. My focus is on delivering high-quality, performant, and scalable apps that meet client and user needs.",
+    ],
+  },
+  {
+    title: "Intern Web Developer",
+    company_name: "Shopify",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "As a Web Developer, I specialize in creating robust, responsive, and visually appealing web applications. My experience spans the full stack, with expertise in both front-end and back-end development, enabling me to deliver end-to-end solutions that align with client objectives.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    icon: meta,
+    iconBg: "#a2d2ff",
+    date: "Jan 2023 - Present",
+    points: [
+      "As a Full Stack Developer, I possess extensive experience in building and managing complete web applications, from designing intuitive front-end interfaces to developing robust back-end systems. I thrive on creating scalable, efficient, and secure solutions that meet both user and business needs.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/fredtinotenda3",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://github.com/fredtinotenda3",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Amazon Price Tracker",
+    description:
+      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
+    link: "https://pricewise-jsm.vercel.app/",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Full Stack Threads Clone",
+    description:
+      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+    link: "https://github.com/fredtinotenda3",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Car Finding App",
+    description:
+      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+    link: "https://github.com/fredtinotenda3",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Full Stack Instagram Clone",
+    description:
+      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+    link: "https://github.com/fredtinotenda3",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Real-Estate Application",
+    description:
+      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
+    link: "https://github.com/fredtinotenda3",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "AI Summarizer Application",
+    description:
+      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+    link: "https://github.com/fredtinotenda3",
+  },
+];
